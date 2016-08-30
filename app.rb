4c26bbf6c8442bbc7aae869834f4e469cd399db2
George Murnaghan Gordon <gordonmgeorge@gmail.com>
2016-08-30 11:24:32 -0400

    first version

--- app.rb
+++ app.rb
@@ -1,5 +1,7 @@
 # If you're using bundler, you will need to add this
+$LOAD_PATH.unshift(File.dirname(__FILE__))
 require 'bundler/setup'
+require 'lib/paper_sizer'
 
 require 'sinatra'
 
@@ -8,5 +10,31 @@ get '/' do
 end
 
 post '/sizeit' do
+  height = params["paper_height"]
+  width  = params["paper_width"]
+  chain  = params["chain_lines"]
+  deckle = params["deckle"]
+  sizer = PaperSizer.new
+
+  matches  = sizer.find_matches height, width
+  if matches.length == 0
+    result = "Those dimensions don't match any known paper size."
+  elsif matches.length  == 1
+    result = matches[0].ouput_name
+  else
+    matches_lines = sizer.query_lines chain
+    if matches_lines.length == 1
+      result = matches_lines[0].output_name
+    else
+      matches_edge = sizer.query_edge deckle
+      if matches_edge.length ==1
+        result = matches_edge[0].output_name
+      else
+        result = sizer.calculate_percent height, width
+      end
+    end
+  end
+
+  params["result"] = result
   erb :sizeit, locals: params
 end
