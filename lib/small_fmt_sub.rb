b1015ae4c4ec9251d918138e0a4bb78e8cca84f7
George Murnaghan Gordon <gordonmgeorge@gmail.com>
2017-01-17 11:47:06 -0500

    removed old format-string system

--- lib/small_fmt_sub.rb
+++ lib/small_fmt_sub.rb
@@ -1,17 +1,3 @@
-class String
-  def small_fmt_sub
-  s = [
-      %w(SIXTEEN-MO   16mo),
-      %w(THIRTYTWO-MO 32mo),
-      %w(SIXTYFOUR-MO 64mo)
-      ].inject(self){ |o, p| o.sub(p[0], p[1]) }
-  s
-  end
-end
-
-
-
-
 def fmt_str(fmt)
   case fmt
   when :sixteen_mo
